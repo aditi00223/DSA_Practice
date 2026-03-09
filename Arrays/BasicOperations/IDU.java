612fb28d9a749af97288db805f7cb19abf518198
Aditi Sharma <aditisharma032007@gmail.com>
2026-03-09 09:55:07 +0530

    Initial commit

---
+++ Arrays/BasicOperations/IDU.java
@@ -0,0 +1,16 @@
+package Arrays.BasicOperations;
+
+class IDU {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5};
+        int index = 2;
+        int newValue = 10;
+
+        arr[index] = newValue;
+
+        System.out.print("Updated array: ");
+        for (int i : arr) {
+            System.out.print(i + " ");
+        }
+    }
+}
